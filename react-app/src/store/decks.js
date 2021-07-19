d9e94e2a29128b61fd4f8140b4a412e7d992d274
Dechon Ryan <dechon@tuta.io>
2021-07-19 16:44:18 -0400

    Start decks store

---
+++ react-app/src/store/decks.js
@@ -0,0 +1,11 @@
+// hit the backend routes for data!
+
+const LOAD = 'decks/LOAD'
+const ADD_ONE = 'decks/ADD_ONE'
+
+const load = (decks) => ({
+    type: 'LOAD',
+    decks
+})
+
+const add_one = ()
